ffc7d2030c45ae31cfdef85eff516eb678c43761
igor-starostenko <contactigorstar@gmail.com>
2017-10-07 15:54:22 -0700

    added configuration module

--- lib/tune_spec.rb
+++ lib/tune_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require 'tune_spec/configuration'
 require 'tune_spec/version'
 
 # Namespace for all TuneSpec objects
 module TuneSpec
+  extend Configuration
   # Your code goes here...
 end
---
+++ lib/tune_spec/configuration.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module TuneSpec
+  # Defines all configurable attributes of TuneSpec
+  module Configuration
+    VALID_CONFIG_KEYS = %i[directory page_opts step_opts group_opts].freeze
+
+    DEFAULT_DIRECTORY = 'lib'
+
+    DEFAULT_PAGE_OPTS = {}.freeze
+
+    DEFAULT_STEP_OPTS = {}.freeze
+
+    DEFAULT_GROUP_OPTS = {}.freeze
+
+    attr_accessor(*VALID_CONFIG_KEYS)
+
+    # Make sure the default values are set when the module is 'extended'
+    def self.extended(base)
+      base.reset
+    end
+
+    def reset
+      VALID_CONFIG_KEYS.each do |key|
+        send("#{key}=", self::Configuration.const_get("DEFAULT_#{key}".upcase))
+      end
+    end
+  end
+end
