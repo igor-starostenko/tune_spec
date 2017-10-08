3f4f6770a35958f54e1b5c1950d253f7c97004d5
igor-starostenko <contactigorstar@gmail.com>
2017-10-08 10:13:41 -0700

    added yard and doctest

--- lib/tune_spec.rb
+++ lib/tune_spec.rb
@@ -6,5 +6,7 @@ require 'tune_spec/version'
 # Namespace for all TuneSpec objects
 module TuneSpec
   extend Configuration
-  # Your code goes here...
+  # TODO: Add method missing
 end
+
+require 'tune_spec/instances'
