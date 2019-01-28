f99c05d7e88ac5f6bc5490564d5c4590eb343820
igor-starostenko <contactigorstar@gmail.com>
2019-01-27 23:55:47 -0800

    rubocop clean up

--- doctest_helper.rb
+++ doctest_helper.rb
@@ -3,8 +3,44 @@
 require 'tune_spec'
 require 'fileutils'
 
-include TuneSpec::Instances
 TEST_ENV = 'STG'
+include TuneSpec::Instances # rubocop:disable Style/MixinUsage
+
+# Dynamically generates test files
+module DoctestHelper
+  def self.create_file(name, type)
+    folder_name = type == :page ? 'pages' : type.to_s
+    file_name = "test/#{folder_name}/#{name}_#{type}.rb"
+    FileUtils.mkdir_p("test/#{folder_name}")
+    File.open(file_name, 'w+') { |f| f << file_content(name, type) }
+  end
+
+  def self.file_content(name, type)
+    <<~FILE
+      class_name = "#{name.capitalize}#{type.capitalize}"
+      klass = Object.const_set(class_name, Class.new)
+
+      klass.class_eval do
+        case "#{type}"
+        when 'groups'
+          define_method('initialize') do |test, env:, aut:|; end
+          define_method('complete') do; end
+        when 'steps'
+          define_method('initialize') do |env:, page_object:|
+            @env = env
+            @page_object = page_object
+          end
+          define_method('verify_result') do
+            @page_object.click_element
+          end
+        when 'page'
+          define_method('initialize') do |env:|; end
+          define_method('click_element') do; end
+        end
+      end
+    FILE
+  end
+end
 
 TuneSpec.configure do |conf|
   conf.directory = 'test'
@@ -14,42 +50,9 @@ TuneSpec.configure do |conf|
   conf.page_opts = { env: TEST_ENV }
 end
 
-def create_file(name, type)
-  folder_name = type == :page ? 'pages' : type.to_s
-  file_name = "test/#{folder_name}/#{name}_#{type}.rb"
-  FileUtils.mkdir_p("test/#{folder_name}")
-  File.open(file_name, 'w+') { |f| f << file_content(name, type) }
-end
-
-def file_content(name, type)
-  <<~FILE
-    class_name = "#{name.capitalize}#{type.capitalize}"
-    klass = Object.const_set(class_name, Class.new)
-
-    klass.class_eval do
-      case "#{type}"
-      when 'groups'
-        define_method('initialize') do |test, env:, aut:|; end
-        define_method('complete') do; end
-      when 'steps'
-        define_method('initialize') do |env:, page_object:|
-          @env = env
-          @page_object = page_object
-        end
-        define_method('verify_result') do
-          @page_object.click_element
-        end
-      when 'page'
-        define_method('initialize') do |env:|; end
-        define_method('click_element') do; end
-      end
-    end
-  FILE
-end
-
-create_file(:login, :groups)
-create_file(:calculator, :steps)
-create_file(:home, :page)
+DoctestHelper.create_file(:login, :groups)
+DoctestHelper.create_file(:calculator, :steps)
+DoctestHelper.create_file(:home, :page)
 
 YARD::Doctest.configure do |doctest|
   doctest.after_run do
